a3b04eb66cb6df86465df5e07834477eace0162e
sosella <stephen.osella@gmail.com>
2020-09-27 16:19:11 -0500

    Answered timeInWords

--- CS/Hackerrank_CS/Answered.cs
+++ CS/Hackerrank_CS/Answered.cs
@@ -4418,6 +4418,134 @@
 #endif
 // ---------------------------------------------------------------------------------------------------------
 #if false
+    class Program
+    {
+        private static string nbrToString(int h)
+        {
+            switch (h)
+            {
+                case 1:
+                    return "one";
+                case 2:
+                    return "two";
+                case 3:
+                    return "three";
+                case 4:
+                    return "four";
+                case 5:
+                    return "five";
+                case 6:
+                    return "six";
+                case 7:
+                    return "seven";
+                case 8:
+                    return "eight";
+                case 9:
+                    return "nine";
+                case 10:
+                    return "ten";
+                case 11:
+                    return "eleven";
+                case 12:
+                    return "twelve";
+                case 13:
+                    return "thirteen";
+                case 14:
+                    return "fourteen";
+                case 15:
+                    return "fifteen";
+                case 16:
+                    return "sixteen";
+                case 17:
+                    return "seventeen";
+                case 18:
+                    return "eighteen";
+                case 19:
+                    return "nineteen";
+                case 20:
+                    return "twenty";
+                case 21:
+                    return "twenty one";
+                case 22:
+                    return "twenty two";
+                case 23:
+                    return "twenty three";
+                case 24:
+                    return "twenty four";
+                case 25:
+                    return "twenty five";
+                case 26:
+                    return "twenty six";
+                case 27:
+                    return "twenty seven";
+                case 28:
+                    return "twenty eight";
+                case 29:
+                    return "twenty nine";
+                case 30:
+                    return "thirty";
+                default:
+                    throw new Exception();
+            }
+        }
+
+        static string timeInWords(int h, int m)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if (m == 0)
+            {
+                sb.Append(nbrToString(h));
+                sb.Append(" o' clock");
+            }
+            else if (m == 1)
+            {
+                sb.Append(nbrToString(m));
+                sb.Append(" minute past ");
+                sb.Append(nbrToString(h));
+            }
+            else if (m == 15)
+            {
+                sb.Append("quarter past ");
+                sb.Append(nbrToString(h));
+            }
+            else if (m <= 29)
+            {
+                sb.Append(nbrToString(m));
+                sb.Append(" minutes past ");
+                sb.Append(nbrToString(h));
+            }
+            else if (m == 30)
+            {
+                sb.Append("half past ");
+                sb.Append(nbrToString(h));
+            }
+            else if (m == 45)
+            {
+                sb.Append("quarter to ");
+                sb.Append(nbrToString(h + 1));
+            }
+            else if (m <= 59)
+            {
+                sb.Append(nbrToString(60 - m));
+                sb.Append(" minutes to ");
+                sb.Append(nbrToString(h + 1));
+            }
+            else
+            {
+                throw new Exception();
+            }
+
+            return sb.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(timeInWords(5, 47));  // thirteen minutes to six
+            Console.WriteLine(timeInWords(3, 0));   // three o' clock
+            Console.WriteLine(timeInWords(7, 15));   // quarter past seven
+        }
+    }
 #endif
 // ---------------------------------------------------------------------------------------------------------
 #if false
